40eb5331e0977f9e2c9188f8f360c935c2f988cb
Uche Ozoemena <ucheozoemena@gmail.com>
2018-12-13 18:06:19 +0100

    Create and render a simple Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import Navbar from './Navbar';
 import Clock from './Clock';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <section>
+        <Navbar title="SnuzeTime" />
         <Clock currentTime="02:49 AM" />
-      </div>
+      </section>
     );
   }
 }
