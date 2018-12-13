7ad14b4153880bb9e61cfae4b19da248672e13ca
Uche Ozoemena <ucheozoemena@gmail.com>
2018-12-13 16:05:35 +0100

    Add styling to Clock component

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,7 +1,16 @@
+import './Clock.css';
 import React from 'react';
 
 const Clock = props => {
-  return <div>{props.currentTime}</div>;
+  const [, time, meridien] = props.currentTime.match(/(\d{2}:\d{2}) (AM|PM)/);
+
+  return (
+    <div className="clock-container">
+      <div className="border">
+        <span className="time">{time}</span> {meridien}
+      </div>
+    </div>
+  );
 };
 
 export default Clock;
