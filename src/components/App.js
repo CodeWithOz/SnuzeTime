769de1eb0386fc8cb13d7ddc3e5bcde29ea48a64
Uche Ozoemena <ucheozoemena@gmail.com>
2018-12-15 21:07:28 +0100

    Activate Clock with current time
    The Clock display has also been tweaked to show
    the seconds in addition to hours and minutes.

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,34 @@ import Navbar from './Navbar';
 import Clock from './Clock';
 import ButtonDisplay from './ButtonDisplay';
 import TodayView from './TodayView';
+import moment from 'moment';
+import 'moment-timer';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // TODO: improve the initial loading experience
+      currentTime: '00:00:00 AM'
+    };
+
+    this.startTimer();
+  }
+
+  startTimer = () => {
+    new moment.duration(1000).timer({ start: true, loop: true }, () => {
+      this.setState({
+        currentTime: moment().format('hh:mm:ss A')
+      });
+    });
+  };
+
   render() {
     return (
       <section>
         <Navbar title="SnuzeTime" />
-        <Clock currentTime="02:49 AM" />
+        <Clock currentTime={this.state.currentTime} />
         <ButtonDisplay
           hour={10}
           saveSleepTime={e => console.log(e)}
--- src/components/Clock.js
+++ src/components/Clock.js
@@ -2,7 +2,9 @@ import './Clock.css';
 import React from 'react';
 
 const Clock = props => {
-  const [, time, meridiem] = props.currentTime.match(/(\d{2}:\d{2}) (AM|PM)/);
+  const [, time, meridiem] = props.currentTime.match(
+    /(\d{2}:\d{2}:\d{2}) (AM|PM)/
+  );
 
   return (
     <div className="clock-container">
