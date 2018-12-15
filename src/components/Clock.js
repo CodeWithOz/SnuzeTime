0cb43c3deb590ce8be233e433a0f80938504bfde
Uche Ozoemena <ucheozoemena@gmail.com>
2018-12-15 20:56:35 +0100

    Fix meridiem spelling

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -2,12 +2,12 @@ import './Clock.css';
 import React from 'react';
 
 const Clock = props => {
-  const [, time, meridien] = props.currentTime.match(/(\d{2}:\d{2}) (AM|PM)/);
+  const [, time, meridiem] = props.currentTime.match(/(\d{2}:\d{2}) (AM|PM)/);
 
   return (
     <div className="clock-container">
       <div className="border">
-        <span className="time">{time}</span> {meridien}
+        <span className="time">{time}</span> {meridiem}
       </div>
     </div>
   );
