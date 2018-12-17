7a2e4300beaeb5872caa9a1d8bd885b40b6e5422
Uche Ozoemena <ucheozoemena@gmail.com>
2018-12-17 06:52:09 +0100

    Test and implement more storage functionality
    This adds the ability to update data in the store.

--- src/helpers/dateStore.js
+++ src/helpers/dateStore.js
@@ -47,8 +47,35 @@ function parseDate(date) {
   return date.match(/^(\d{4}) (\d{1,2}) (\d{1,2})$/);
 }
 
+function add(date, times, store) {
+  const [, year, month, day] = parseDate(date);
+
+  if (store.has(year) && store.get(year).has(month)) {
+    // records available for this month so add day
+    // .set() will overwrite the data for the current day if they exist
+    // or add them if they don't
+    // which is the behaviour we want
+    store
+      .get(year)
+      .get(month)
+      .set(day, times);
+  } else if (store.has(year)) {
+    // has records for year but not month
+    // so create the day and add it to the month
+    const newMonth = new Map([[day, times]]);
+    store.get(year).set(month, newMonth);
+  } else {
+    // doesn't have records for the year
+    // so create the record and add it
+    const newMonth = new Map([[day, times]]);
+    const newYear = new Map([[month, newMonth]]);
+    store.set(year, newYear);
+  }
+}
+
 export default {
   mapToString,
   stringToMap,
-  find
+  find,
+  add
 };
--- src/helpers/dateStore.test.js
+++ src/helpers/dateStore.test.js
@@ -1,15 +1,19 @@
 import dateStore from './dateStore';
 
-const mockDay = {
-  sleepTime: '10:30 PM',
-  wakeTime: '06:00 AM',
-  getUpTime: '06:30 AM'
-};
-const mockOctober = new Map([['6', mockDay]]); // October 6
-const mock2018 = new Map([['10', mockOctober]]);
-const mockStore = new Map([['2018', mock2018]]);
 const dateWithData = '2018 10 6';
 const dateWithoutData = '2018 4 2';
+let mockDay, mockOctober, mock2018, mockStore;
+
+beforeEach(() => {
+  mockDay = {
+    sleepTime: '10:30 PM',
+    wakeTime: '06:00 AM',
+    getUpTime: '06:30 AM'
+  };
+  mockOctober = new Map([['6', mockDay]]); // October 6
+  mock2018 = new Map([['10', mockOctober]]);
+  mockStore = new Map([['2018', mock2018]]);
+});
 
 describe('Converting from Map to string and back', () => {
   test('produces an identical Map', () => {
@@ -38,5 +42,136 @@ describe('Finding data in the Map', () => {
     expect(sleepTime).toEqual('');
     expect(wakeTime).toEqual('');
     expect(getUpTime).toEqual('');
+  });
+});
+
+describe('Updating the Map', () => {
+  describe('with a date in a new year', () => {
+    let newDate, newTimes;
+
+    beforeEach(() => {
+      newDate = '2019 9 18';
+      newTimes = {
+        sleepTime: '09:50PM',
+        wakeTime: '04:30 AM',
+        getUpTime: '05:00 AM'
+      };
+    });
+
+    test('adds the supplied info correctly', () => {
+      dateStore.add(newDate, newTimes, mockStore);
+      const { sleepTime, wakeTime, getUpTime } = dateStore.find(
+        newDate,
+        mockStore
+      );
+      expect(sleepTime).toEqual(newTimes.sleepTime);
+      expect(wakeTime).toEqual(newTimes.wakeTime);
+      expect(getUpTime).toEqual(newTimes.getUpTime);
+    });
+
+    test(`doesn't overwrite previous data`, () => {
+      dateStore.add(newDate, newTimes, mockStore);
+      const { sleepTime, wakeTime, getUpTime } = dateStore.find(
+        dateWithData,
+        mockStore
+      );
+      expect(sleepTime).toEqual(mockDay.sleepTime);
+      expect(wakeTime).toEqual(mockDay.wakeTime);
+      expect(getUpTime).toEqual(mockDay.getUpTime);
+    });
+  });
+
+  describe('with a date in a new month', () => {
+    let newDate, newTimes;
+
+    beforeEach(() => {
+      newDate = '2018 9 18';
+      newTimes = {
+        sleepTime: '10:50PM',
+        wakeTime: '06:30 AM',
+        getUpTime: '07:00 AM'
+      };
+    });
+
+    test('adds the supplied info correctly', () => {
+      dateStore.add(newDate, newTimes, mockStore);
+      const { sleepTime, wakeTime, getUpTime } = dateStore.find(
+        newDate,
+        mockStore
+      );
+      expect(sleepTime).toEqual(newTimes.sleepTime);
+      expect(wakeTime).toEqual(newTimes.wakeTime);
+      expect(getUpTime).toEqual(newTimes.getUpTime);
+    });
+
+    test(`doesn't overwrite previous data`, () => {
+      dateStore.add(newDate, newTimes, mockStore);
+      const { sleepTime, wakeTime, getUpTime } = dateStore.find(
+        dateWithData,
+        mockStore
+      );
+      expect(sleepTime).toEqual(mockDay.sleepTime);
+      expect(wakeTime).toEqual(mockDay.wakeTime);
+      expect(getUpTime).toEqual(mockDay.getUpTime);
+    });
+  });
+
+  describe('with a new day', () => {
+    let newDate, newTimes;
+
+    beforeEach(() => {
+      newDate = '2018 10 7';
+      newTimes = {
+        sleepTime: '11:50PM',
+        wakeTime: '07:30 AM',
+        getUpTime: '08:00 AM'
+      };
+    });
+
+    test('adds the supplied info correctly', () => {
+      dateStore.add(newDate, newTimes, mockStore);
+      const { sleepTime, wakeTime, getUpTime } = dateStore.find(
+        newDate,
+        mockStore
+      );
+      expect(sleepTime).toEqual(newTimes.sleepTime);
+      expect(wakeTime).toEqual(newTimes.wakeTime);
+      expect(getUpTime).toEqual(newTimes.getUpTime);
+    });
+
+    test(`doesn't overwrite previous data`, () => {
+      dateStore.add(newDate, newTimes, mockStore);
+      const { sleepTime, wakeTime, getUpTime } = dateStore.find(
+        dateWithData,
+        mockStore
+      );
+      expect(sleepTime).toEqual(mockDay.sleepTime);
+      expect(wakeTime).toEqual(mockDay.wakeTime);
+      expect(getUpTime).toEqual(mockDay.getUpTime);
+    });
+  });
+
+  describe('with a currently recorded day', () => {
+    let newTimes;
+
+    beforeEach(() => {
+      newTimes = {
+        sleepTime: '11:50PM',
+        wakeTime: '07:30 AM',
+        getUpTime: '08:00 AM'
+      };
+    });
+
+    test(`overwrites the previous data`, () => {
+      dateStore.add(dateWithData, newTimes, mockStore);
+      const { sleepTime, wakeTime, getUpTime } = dateStore.find(
+        dateWithData,
+        mockStore
+      );
+
+      expect(sleepTime).toEqual(newTimes.sleepTime);
+      expect(wakeTime).toEqual(newTimes.wakeTime);
+      expect(getUpTime).toEqual(newTimes.getUpTime);
+    });
   });
 });
