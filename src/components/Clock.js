5a5dd33523ab68a4398aee5897d483b33a76f3a8
Uche Ozoemena <ucheozoemena@gmail.com>
2018-12-23 00:28:48 +0100

    Add features to Clock using Grommet
    Features include:
    - Accessibility titles
    - Different sizes for time and meridiem
    - Rounded border

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,6 +1,6 @@
 import './Clock.css';
 import React from 'react';
-import { Box } from 'grommet';
+import { Box, Text } from 'grommet';
 
 const Clock = props => {
   const [, time, meridiem] = props.currentTime.match(
@@ -11,7 +11,12 @@ const Clock = props => {
     <Box alignSelf="center" pad="medium">
       <div className="clock-container">
         <div className="border">
-          <span className="time">{time}</span> {meridiem}
+          <Text className="time" a11yTitle="time" size="large">
+            {time}
+          </Text>{' '}
+          <Text a11yTitle="AM or PM" size="small">
+            {meridiem}
+          </Text>
         </div>
       </div>
     </Box>
