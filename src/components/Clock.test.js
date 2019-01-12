8f7feab9c3281613af5a051b5618ac34ceb0efa3
Uche Ozoemena <ucheozoemena@gmail.com>
2019-01-12 05:59:53 +0100

    Add test for Clock component

---
+++ src/components/Clock.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'enzyme';
+import Clock from './Clock';
+
+test('Clock displays the supplied time', () => {
+  const time = '09:09:23 PM';
+  const wrapper = render(<Clock currentTime={time} />);
+  expect(wrapper.text()).toContain(time);
+});
