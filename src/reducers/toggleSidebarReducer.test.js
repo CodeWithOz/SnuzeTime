bc56a87a5006c3b00d78c63582d73a5e623a23c4
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-20 11:45:05 +0100

    Test if toggleSiderbarReducer exists

---
+++ src/reducers/toggleSidebarReducer.test.js
@@ -0,0 +1,7 @@
+import toggleSidebarReducer from './toggleSidebarReducer';
+
+describe('toggleSidebarReducer', () => {
+  test('is a function', () => {
+    expect(typeof toggleSidebarReducer).toEqual('function');
+  });
+});
