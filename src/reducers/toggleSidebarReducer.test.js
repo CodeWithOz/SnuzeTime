d8f0c94c3a165f7478f00b250706053f7d55bc95
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-20 11:57:12 +0100

    Test default state of sidebar reducer

--- src/reducers/toggleSidebarReducer.test.js
+++ src/reducers/toggleSidebarReducer.test.js
@@ -1,7 +1,16 @@
 import toggleSidebarReducer from './toggleSidebarReducer';
+import { INITIAL_STATE } from '../constants';
 
 describe('toggleSidebarReducer', () => {
   test('is a function', () => {
     expect(typeof toggleSidebarReducer).toEqual('function');
+  });
+
+  describe('returns', () => {
+    test('the initial state by default', () => {
+      // no type key in action so it doesn't match reducer
+      const state = toggleSidebarReducer(undefined, {});
+      expect(state).toEqual(INITIAL_STATE.sidebarShown);
+    });
   });
 });
