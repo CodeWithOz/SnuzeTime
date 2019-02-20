09c821f0793db3fca747af49830ad0aa3664a077
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-20 12:41:56 +0100

    Handle matching action in sidebar reducer

--- src/reducers/toggleSidebarReducer.js
+++ src/reducers/toggleSidebarReducer.js
@@ -1,7 +1,10 @@
-import { INITIAL_STATE } from '../constants';
+import { INITIAL_STATE, TOGGLE_SIDEBAR } from '../constants';
 
-const toggleSidebarReducer = (sidebarShown = INITIAL_STATE.sidebarShown) => {
-  return sidebarShown;
+const toggleSidebarReducer = (
+  sidebarShown = INITIAL_STATE.sidebarShown,
+  { type, payload }
+) => {
+  return type === TOGGLE_SIDEBAR ? payload : sidebarShown;
 };
 
 export default toggleSidebarReducer;
