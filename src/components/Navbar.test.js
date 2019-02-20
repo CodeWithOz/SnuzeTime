c8ac9009184b8312f7ae54827c51dd0bef90b110
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-20 18:27:17 +0100

    Test if Navbar displays a menu icon

--- src/components/Navbar.test.js
+++ src/components/Navbar.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { Menu } from 'grommet-icons';
 import Navbar from './Navbar';
 
-test('Navbar displays the supplied title', () => {
-  const title = 'test title';
-  const wrapper = mount(<Navbar title={title} />);
-  expect(wrapper.text()).toContain(title);
+describe('Navbar', () => {
+  test('displays the supplied title', () => {
+    const title = 'test title';
+    const wrapper = mount(<Navbar title={title} />);
+    expect(wrapper.text()).toContain(title);
+  });
+
+  test('displays a grommet menu icon', () => {
+    const wrapper = mount(<Navbar />);
+    expect(wrapper.find(Menu).length).toEqual(1);
+  });
 });
