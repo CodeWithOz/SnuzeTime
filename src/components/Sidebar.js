6955c6df95bd9fd413b210d8bab05b70c8562740
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-21 17:02:33 +0100

    Return null instead of false
    React complains if nothing is rendered, as happens
    when the render method returns false.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { Layer } from 'grommet';
 
 const Sidebar = ({ shown }) => {
-  return (
-    shown && (
-      <Layer full="vertical" position="left">
-        Sidebar
-      </Layer>
-    )
-  );
+  return shown ? (
+    <Layer full="vertical" position="left">
+      Sidebar
+    </Layer>
+  ) : null;
 };
 
 export default Sidebar;
