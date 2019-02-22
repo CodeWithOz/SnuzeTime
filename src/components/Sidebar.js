229046f38c4a3e57fe270e8dc8b6226f8224b2cf
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-22 16:50:53 +0100

    Render a grommet Close icon

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Layer } from 'grommet';
+import { Layer, Box } from 'grommet';
+import { Close } from 'grommet-icons';
 import { toggleSidebar } from '../actions';
 
 export const Sidebar = ({ shown, hide }) => {
   return shown ? (
     <Layer full="vertical" position="left" onEsc={hide} onClickOutside={hide}>
+      <Box
+        direction="horizontal"
+        align="center"
+        justify="end"
+        elevation="small"
+      >
+        <Close />
+      </Box>
       Sidebar
     </Layer>
   ) : null;
