327a613d26a85bc36a9b6932728610c69da33c60
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-22 16:35:42 +0100

    Pass hide callback to Layer appropriately

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,21 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Layer } from 'grommet';
+import { toggleSidebar } from '../actions';
 
-export const Sidebar = ({ shown }) => {
+export const Sidebar = ({ shown, hide }) => {
   return shown ? (
-    <Layer full="vertical" position="left">
+    <Layer full="vertical" position="left" onEsc={hide} onClickOutside={hide}>
       Sidebar
     </Layer>
   ) : null;
 };
 
 Sidebar.propTypes = {
-  shown: PropTypes.bool
+  shown: PropTypes.bool,
+  hide: PropTypes.func
 };
 
 const mapStateToProps = ({ sidebarShown }) => {
   return { shown: sidebarShown };
 };
 
-export default connect(mapStateToProps)(Sidebar);
+export default connect(
+  mapStateToProps,
+  { hide: toggleSidebar }
+)(Sidebar);
