f98504935c56df6909ccc37083fa9040bbfa0f9b
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-22 10:34:55 +0100

    Connect toggleSidebarReducer to redux store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import updateSnuzeTimesReducer from './updateSnuzeTimesReducer';
 import updateCurrentTimesReducer from './updateCurrentTimesReducer';
 import showMainAppReducer from './showMainAppReducer';
 import showTodayViewReducer from './showTodayViewReducer';
+import toggleSidebarReducer from './toggleSidebarReducer';
 
 export default combineReducers({
   snuzeTimes: updateSnuzeTimesReducer,
   currentTimes: updateCurrentTimesReducer,
   mainAppShown: showMainAppReducer,
-  todayViewShown: showTodayViewReducer
+  todayViewShown: showTodayViewReducer,
+  toggleSidebar: toggleSidebarReducer
 });
