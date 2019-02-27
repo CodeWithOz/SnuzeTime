ed6e1aa0705ae918d9203e588746aa0fa08c7606
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-27 17:34:57 +0100

    Pass 'hide' as a prop to SidebarItem

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -14,7 +14,7 @@ export class Sidebar extends Component {
   renderItems() {
     return sidebarConfig.items.map((item, index) => {
       return (
-        <SidebarItem key={index} onClick={this.props.hide} dest={item.dest}>
+        <SidebarItem key={index} handleClick={this.props.hide} dest={item.dest}>
           {item.text}
         </SidebarItem>
       );
