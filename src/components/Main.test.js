30fc44a074b9c5fd1eb942c0efd3925b11d513eb
Uche Ozoemena <ucheozoemena@gmail.com>
2019-03-01 16:54:06 +0100

    Test if Main renders a Route for SelectDay

--- src/components/Main.test.js
+++ src/components/Main.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Route } from 'react-router-dom';
 import Main from './Main';
+import SelectDay from './SelectDay';
 import Today from './Today';
 
 describe('Main', () => {
@@ -10,6 +11,12 @@ describe('Main', () => {
       const wrapper = shallow(<Main />);
       expect(wrapper.find({ component: Today }).length).toEqual(1);
       expect(wrapper.find({ component: Today }).is(Route)).toEqual(true);
+    });
+
+    test('whose component is SelectDay', () => {
+      const wrapper = shallow(<Main />);
+      expect(wrapper.find({ component: SelectDay }).length).toEqual(1);
+      expect(wrapper.find({ component: SelectDay }).is(Route)).toEqual(true);
     });
   });
 });
