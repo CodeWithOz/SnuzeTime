9b04fd2a0d00e6450ed8bc60e878eada0f91e23b
Uche Ozoemena <ucheozoemena@gmail.com>
2019-03-02 07:09:53 +0100

    Test if renderDateHeading handles no date

--- src/components/SelectDay.test.js
+++ src/components/SelectDay.test.js
@@ -1,9 +1,18 @@
+import { shallow } from 'enzyme';
 import SelectDay from './SelectDay';
 
 describe('SelectDay', () => {
   describe('exposes renderDateHeading which', () => {
     test('is a function', () => {
       expect(typeof SelectDay.prototype.renderDateHeading).toEqual('function');
+    });
+
+    describe('renders', () => {
+      const defaultText = 'No date selected';
+      test('default text when no date is supplied', () => {
+        const wrapper = shallow(SelectDay.prototype.renderDateHeading(''));
+        expect(wrapper.text()).toContain(defaultText);
+      });
     });
   });
 });
