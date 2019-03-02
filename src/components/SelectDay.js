40bc187802365daaa76256256641b186de335f81
Uche Ozoemena <ucheozoemena@gmail.com>
2019-03-02 07:42:15 +0100

    Show link to pick date
    Some styling has also been added.

--- src/components/SelectDay.js
+++ src/components/SelectDay.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Box, Heading } from 'grommet';
+import { Anchor, Box, Heading, Text } from 'grommet';
 
 const selectDayConfig = {
   defaultDateHeading: 'No date selected'
@@ -10,7 +10,18 @@ class SelectDay extends Component {
     const renderedDate = date || selectDayConfig.defaultDateHeading;
     return (
       <Box fill align="center">
-        <Heading level="2">{renderedDate}</Heading>
+        <Heading
+          level="2"
+          textAlign="center"
+          margin={{
+            bottom: 'small'
+          }}
+        >
+          {renderedDate}
+        </Heading>
+        <Text size="small" textAlign="center">
+          <Anchor>Pick a date</Anchor>
+        </Text>
       </Box>
     );
   }
