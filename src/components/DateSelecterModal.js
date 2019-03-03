457d72f8d1e61fcdb585d5b632286196eb8e7bf3
Uche Ozoemena <ucheozoemena@gmail.com>
2019-03-03 17:28:42 +0100

    Display a close icon inside DateSelecterModal

--- src/components/DateSelecterModal.js
+++ src/components/DateSelecterModal.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import { Layer } from 'grommet';
+import { Anchor, Box, Layer } from 'grommet';
+import { Close } from 'grommet-icons';
 
 const DateSelecterModal = () => {
-  return <Layer responsive={false}>Date picker</Layer>;
+  return (
+    <Layer responsive={false}>
+      <Box fill background="dark-1">
+        <Box
+          width="medium"
+          direction="row"
+          align="center"
+          justify="end"
+          elevation="xsmall"
+        >
+          <Anchor icon={<Close />} />
+        </Box>
+      </Box>
+    </Layer>
+  );
 };
 
 export default DateSelecterModal;
