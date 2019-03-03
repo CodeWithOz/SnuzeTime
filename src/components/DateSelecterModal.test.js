a18acffb5b800853ba7d1d4a3ae26cc12f921e85
Uche Ozoemena <ucheozoemena@gmail.com>
2019-03-03 17:15:35 +0100

    Test if modal renders a grommet Close icon

--- src/components/DateSelecterModal.test.js
+++ src/components/DateSelecterModal.test.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Layer } from 'grommet';
+import { Anchor, Layer } from 'grommet';
+import { Close } from 'grommet-icons';
 import DateSelecterModal from './DateSelecterModal';
 
 describe('DateSelecterModal', () => {
   describe('renders', () => {
+    const wrapper = shallow(<DateSelecterModal />);
     test('a grommet Layer', () => {
-      const wrapper = shallow(<DateSelecterModal />);
       expect(wrapper.find(Layer).length).toEqual(1);
+    });
+
+    test('a grommet Close icon inside an Anchor', () => {
+      expect(wrapper.find({ icon: <Close /> }).length).toEqual(1);
+      expect(wrapper.find({ icon: <Close /> }).is(Anchor)).toEqual(true);
     });
   });
 });
