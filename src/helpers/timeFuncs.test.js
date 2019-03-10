fdfb6ac4b449ef1960843c6b4162acb0540e1e50
Uche Ozoemena <ucheozoemena@gmail.com>
2019-03-10 22:37:04 +0100

    Test functionality of getCurrentHour

--- src/helpers/timeFuncs.test.js
+++ src/helpers/timeFuncs.test.js
@@ -1,9 +1,17 @@
+import moment from 'moment';
 import { getCurrentHour } from './timeFuncs';
 
 describe('Module exposes', () => {
   describe('getCurrentHour which', () => {
     test('is a function', () => {
       expect(typeof getCurrentHour).toEqual('function');
+    });
+
+    test('returns current hour in 24-hour format', () => {
+      const hour = getCurrentHour();
+
+      const timeFromMoment = Number(moment().format(`HH`));
+      expect(hour).toEqual(timeFromMoment);
     });
   });
 });
